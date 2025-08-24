1eda36712b86c49a00f5e9e6f64462be12863e0c
Satyam Prajapati <mrstm.cpp@gmail.com>
2025-08-24 13:02:20 +0530

    aws backend

--- frontend/src/utils/config/backendUrl.js
+++ frontend/src/utils/config/backendUrl.js
@@ -1,2 +1,2 @@
-export const backendURL = "https://mrstmrecords.onrender.com/api/v1";
-export const socketURL = "http://localhost:5500/"
+export const backendURL = "http://13.201.87.134:5500/api/v1";
+export const socketURL = "http://13.201.87.134:5500/"
